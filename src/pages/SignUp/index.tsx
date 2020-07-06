abef4e82e70f9633427a0303d3171a4d4a0d3b2b
Denis Souza <denyssouza1@hotmail.com>
2020-07-05 23:48:17 -0300

    validations forms

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
   FiArrowLeft, FiMail, FiLock, FiUser,
 } from 'react-icons/fi';
 
+import * as Yup from 'yup';
+
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -13,18 +18,34 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: Object): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string().required('Email obrigatório').email('Digite um e-mail valido'),
+          password: Yup.string().min(6, 'No mínimo 6 digitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
-          <Input icon={FiUser} name="nome" type="text" placeholder="Nome" />
+          <Input icon={FiUser} name="name" type="text" placeholder="Nome" />
           <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
           <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
           <Button type="submit">Cadastrar</Button>
